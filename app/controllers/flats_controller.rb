a9bb2f8e4a2cc69a910de17a86946b4f522092cf
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-23 17:34:04 +0800

    got to the show flat part

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,27 @@
 class FlatsController < ApplicationController
-def index
-  @flats = Flat.all
-end
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.new(flats_params)
+    if @flat.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def flats_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :flats
+  root "flats#index"
+  resources :flats, except: :index
 end
