f3b77bdfa45f24caa81e344dd36f26a1132a5da6
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-24 16:25:20 +0800

    airbnb challenge done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,25 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flats_params)
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
   private
   def flats_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
